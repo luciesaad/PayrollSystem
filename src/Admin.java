1890c89972739f13955a3c70379480aff6ea6e5f
Lucie Saad <lucie.saad@outlook.com>
2020-01-29 14:20:24 +0100

    fixed eror in Admin - getUsers arraylist

--- src/Admin.java
+++ src/Admin.java
@@ -56,9 +56,9 @@ public class Admin extends Account implements MenuInterFace {
      }
 
      public String printUserName(String user){
-        for(int i = 0; i <users.size(); i++){
-            if(users.get(i).getUsername().equals(user)){
-                return users.get(i).getUsername();
+        for(int i = 0; i <Admin.getUsers().size(); i++){
+            if(Admin.getUsers().get(i).getUsername().equals(user)){
+                return Admin.getUsers().get(i).getUsername();
             }
         }
          return "Cant find user";
