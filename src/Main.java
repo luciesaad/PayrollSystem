4b4913abb9c8661b49a7b32f27d24b8655d58fea
madde1 <maddeswish@gmail.com>
2020-01-30 10:51:22 +0100

    updated to do list

--- src/Main.java
+++ src/Main.java
@@ -12,10 +12,7 @@ public class Main {
 
     /**To do list for now!*/
     //TODO: finish all tests in MenuListAndHandleUsersTest
-    //TODO: finish delete method in menuListAndHandleUsers
     //TODO: create a handler for salary request and role request in admin coming from user
-    //TODO: create Log out function for admin and user
-    //TODO: create method that pays salary to users
     //TODO: finsih all the test
     //TODO: Refactor and optimise code.
     //TODO: Java doc and comment
