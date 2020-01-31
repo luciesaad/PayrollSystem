04b9387745a3390b597647784b19a11850777544
madde1 <maddeswish@gmail.com>
2020-01-31 11:45:50 +0100

    test to test all the test classes at the same time

--- Test/MainTest.java
+++ Test/MainTest.java
@@ -1,16 +1,18 @@
 import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
 
 import static org.junit.Assert.*;
 
 public class MainTest {
 
     @Test
-    public void userName() {
-    }
-
-    @Test
-    public void login() {
-        Main main = new Main();
-
+    public void test() {
+        Result result = JUnitCore.runClasses(AdminTest.class,CreateUserLogicTest.class, MenuHandleUsersTest.class,MenuLogicTest.class, StartProgramTest.class, UserTest.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        System.out.println(result.wasSuccessful());
     }
 }
